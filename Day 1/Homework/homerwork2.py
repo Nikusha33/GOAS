412329e8289e46960dc0898c2af1c858c3876339
Nikusha33 <nikusha399@gmail.com>
2024-05-06 15:05:35 +0400

    Goa

---
+++ Day 1/Homework/homerwork2.py
@@ -0,0 +1,22 @@
+name = "nikusha"
+
+surname = "shalamberidze"
+
+#print(name)
+
+name = "nikusha"
+age = 16
+height = 174.5
+
+knows_programming = True #True ან False
+is_ugly = False
+
+isUgly = False
+
+print("mevar " + name+ " " + surname+ " " + str(age) + " wlis" + " " + str(height) + " simagleshi")
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+(print(type(knows_programming)))
