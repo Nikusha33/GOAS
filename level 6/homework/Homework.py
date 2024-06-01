9af22b61e8470fb1390b74226ca7f447be78d370
Nikusha33 <nikusha399@gmail.com>
2024-06-02 01:16:37 +0400

    commit

---
+++ level 6/homework/Homework.py
@@ -0,0 +1,28 @@
+#დავალება მეორე
+num1 = int(input("enter your number 1:"))
+num2 = int(input("enter your number 2:"))
+print(num1 + num2)
+print(num1 * num2)
+print(num1 - num2 )
+print(num1 / num2)
+
+#დავალება მესამე
+name = input("enter your name:")
+print(" gilocavt dabadebis dges" + "  "+ name)
+
+#დავალება მეოთხე
+
+name = input("enter your name:") 
+surname = input("enter your surname:")
+age = input("enter your age:")
+print(name, surname, age)
+
+#დავალება მეხუთე
+mom = int(input("40:"))
+dad = int(input("45:"))
+sister = int(input(":"))
+brother = int(input("28:"))
+print(mom + 25)
+print(dad + 25)
+print(sister + 25)
+print(brother + 25)
--- level 6/homework/classwork.py
+++
@@ -1,4 +0,0 @@
-numberN1 = 11
-numberN2 = 9
-
-print(input(numberN1) + input(numberN2))
