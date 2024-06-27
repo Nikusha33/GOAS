cf796ac9e4b67f242fb32589c467c92a024f1dc5
Nikusha33 <nikusha399@gmail.com>
2024-06-27 14:30:54 +0400

    level 10

---
+++ level 10/homework/homework.py
@@ -0,0 +1,32 @@
+#1
+for i in range (5):
+    print("hello")
+
+#2
+for i in range(0,100,2):
+  print("GOA")
+
+#3  
+for i in range (0,30,3):
+   print("Hi")
+#4   
+for i in range(20,25):
+   print("Academy")
+#5
+for i in range(0,11):
+   print( str(i) + ". hello")
+#6
+for i in range(0,7,2):
+   print("hesoyam") 
+#7
+for i in range(33,44,10):
+   print("stop!")
+#8
+for i in range(11,42,15):
+   print(str(i) + "hi")
+#9
+for i in range (17,28,4):
+   print("NIKUSHA")
+#10
+for i in range (11,111,11):
+   print("SHALAMBERIDZE")
